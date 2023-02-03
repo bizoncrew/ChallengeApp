254e6bda3fd155171c117ed5e120b54bf494436a
Bizoncrew@gmail.com <Bizoncrew@gmail.com>
2023-02-04 00:17:48 +0100

    Zadanie dzień 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Marek";
+var gender = "kobieta";
+int age = 17;
+
+if (name == "Marek" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if ( gender == "kobieta" && age < 30)
+{ 
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else
+{
+    Console.WriteLine("Ewa lat 33");
+}
+
+Console.WriteLine($"{name} to nie {gender} w wieku {age} lat!");
