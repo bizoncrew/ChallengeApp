42cfcea0c73603f338589a432d483ad85f1b97c0
Bizoncrew@gmail.com <Bizoncrew@gmail.com>
2023-02-07 22:24:56 +0100

    Zadanie z dnia 5

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,18 +1,28 @@
-﻿var name = "Marek";
-var gender = "kobieta";
-int age = 17;
+﻿int number = 122666;
+var numberAsString = number.ToString();     //zamiana liczby na tekst
+char[] digit = numberAsString.ToArray();    //rozbicie tekstu na pojedyńcze elementy
 
-if (name == "Marek" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else if ( gender == "kobieta" && age < 30)
-{ 
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else
-{
-    Console.WriteLine("Ewa lat 33");
-}
+Console.WriteLine($"Cyfry z liczby {number} występują w następującej ilości:");
 
-Console.WriteLine($"{name} to nie {gender} w wieku {age} lat!");
+List<char> column1 = new List<char>();      //lista cyfr do porównania
+column1.Add('0');
+column1.Add('1');
+column1.Add('2');
+column1.Add('3');
+column1.Add('4');
+column1.Add('5');
+column1.Add('6');
+column1.Add('7');
+column1.Add('8');
+column1.Add('9');
+
+foreach (var element in column1)        //pętla elementów z listy column1
+{
+    int column2 = 0;                    //zmienna początkowa do kolumny drugiej
+    foreach (var character in digit)    //pętla elementów z tablicy char digit
+    {
+        if (element == character)       //jeżeli  element z listy tworzącej pierwszą pętlę = elementowi z tablicy char
+            column2++;                  //powiększ o 1 zmienną początkową z kolumny drugiej
+    }
+    Console.WriteLine(element + " => " + column2);
+}
